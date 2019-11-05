82decb928cb9626a1cb6f749e1bd113da42f202a
PawankumarPK <pk9910765616@gmail.com>
2019-11-05 15:45:46 +0530

    add more function with api

--- PythonToNode/amroAPI.js
+++ PythonToNode/amroAPI.js
@@ -4,31 +4,71 @@ var app = express()
 const sqlite3 = require('sqlite3').verbose();
 // open the database
 let db = new sqlite3.Database('sar.db');
+var params = []
+
 //var path = " ";
 
 function declareAllFunctions() {
 
-    device_stats()
-    amro_stock()
-    face_tracking()
-    amro_trips()
-    floors()
-    rooms()
-    users()
+  device_stats()
+  amro_stock()
+  face_tracking()
+  amro_trips()
+  floors()
+  rooms()
+  get_user_from_pin()
+  get_stock_from_tripId()
+  get_floor_and_rooms()
+  get_trip_status()
+  //get_stock_from_barcode()
+  get_user_from_pin()
+  move()
 }
 
 //Api created
-function device_stats() {
-  app.get("/device_stats", (req, res) => {
-    let sql = `SELECT CreatedOn FROM device_stats`;
-    var params = []
+
+function get_stock_from_tripId() {
+  app.get("/amro-stock", (req, res) => {
+    let sql = 'SELECT stock.Id,stock.Name,amro_stock.Quantity,TripId FROM amro_stock LEFT JOIN stock ON stock.Id=amro_stock.StockId WHERE TripId=?', t;
     db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({ "error": err.message })
         return;
       }
       res.json({
-        "message": "Success",
+        "data": rows
+      })
+    });
+  });
+
+}
+
+function get_floor_and_rooms(){
+  app.get("/floors-rooms", (req, res) => {
+    let sql = 'SELECT rooms.Id,rooms.Name,floors.Id,floors.Name FROM rooms LEFT JOIN floors ON floors.Id=rooms.FloorId'
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "data": rows
+      })
+    });
+  });
+
+}
+
+
+function device_stats() {
+  app.get("/device_stats", (req, res) => {
+    let sql = `SELECT Name,Value FROM device_stats`;
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
         "data": rows
       })
     });
@@ -39,14 +79,12 @@ function device_stats() {
 function amro_stock() {
   app.get("/amro_stock", (req, res) => {
     let sql = `SELECT CreatedOn FROM amro_stock`;
-    var params = []
     db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({ "error": err.message })
         return;
       }
       res.json({
-        "message": "Success",
         "data": rows
       })
     })
@@ -56,15 +94,13 @@ function amro_stock() {
 
 function face_tracking() {
   app.get("/face_tracking", (req, res) => {
-    let sql = `SELECT CreatedOn FROM face_tracking`;
-    var params = []
+    let sql = `SELECT X,Y FROM face_tracking ORDER BY Id DESC LIMIT 1 `;
     db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({ "error": err.message })
         return;
       }
       res.json({
-        "message": "Success",
         "data": rows
       })
     })
@@ -74,15 +110,13 @@ function face_tracking() {
 
 function amro_trips() {
   app.get("/amro_trips", (req, res) => {
-    let sql = `SELECT CreatedOn FROM amro_trips`;
-    var params = []
+    let sql = `SELECT Id,StatusId FROM amro_trips ORDER BY Id DESC LIMIT 1;`;
     db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({ "error": err.message })
         return;
       }
       res.json({
-        "message": "Success",
         "data": rows
       })
     })
@@ -91,26 +125,150 @@ function amro_trips() {
 
 function floors() {
   app.get("/floors", (req, res) => {
-    let sql = `SELECT CreatedOn FROM  floors`;
-    var params = []
+    let sql = `SELECT Name,id FROM  floors`;
     db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({ "error": err.message })
         return;
       }
       res.json({
-        "message": "Success",
-        "data": rows
+        "floors": rows
       })
     })
   })
 }
 
 
-function rooms() {
+function rooms(floorId) {
   app.get("/rooms", (req, res) => {
-    let sql = `SELECT CreatedOn FROM  rooms`;
-    var params = []
+    let sql = `SELECT rooms.Id,rooms.Name FROM rooms WHERE  FloorId = ?`;
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "rooms": rows
+      })
+    })
+  })
+}
+
+function get_trip_status(){
+  app.get("/amro-state", (req, res) => {
+    let sql = (`SELECT * FROM stock WHERE barcode=?`);
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "rooms": rows
+      })
+    })
+  })
+}
+
+function get_stock_from_barcode(barcode){
+  t = (barcode)
+  app.get("/rooms", (req, res) => {
+    let sql = (`SELECT * FROM stock WHERE barcode=?`,t);
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "rooms": rows
+      })
+    })
+  })
+}
+
+function get_user_from_pin() {
+  app.get("/dispatch", (req, res) => {
+    let sql = `select Id,Name from users where Passcode=?`;
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "data": rows
+      })
+    })
+  })
+}
+
+function set_trip_goal( tripId, roomId){
+  p = (roomId,tripId)
+  app.patch("/set-goal-xy", (req, res) => {
+    let sql = ('UPDATE amro_trips SET RoomId=? where Id=?;', p);
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "data": rows
+      })
+    })
+  })
+}
+
+function set_trip_goal_xy(tripId, x, y){
+  p = (-1, str(x) + "|" + str(y), tripId)
+  app.patch("/set-goal-xy", (req, res) => {
+    let sql = ('UPDATE amro_trips SET RoomId=?,RoomXY=? where Id=?;', p);
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "data": rows
+      })
+    })
+  })
+}
+function start_amro_trip(userId, tripId){
+  tripdata = (userId,30,tripId);
+  app.patch("/dispatch", (req, res) => {
+    let sql = ('UPDATE amro_trips SET InitiatedBy=?,StatusId=? where Id=?',tripdata);
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "data": rows
+      })
+    })
+  })
+
+}
+
+function end_amro_trip(userId, tripId){
+  tripdata = (userId,90,tripId)
+  app.patch("/delivery", (req, res) => {
+    let sql = ('UPDATE amro_trips SET RecievedBy=?,StatusId=? where Id=?;',tripdata);
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "data": rows
+      })
+    })
+  })
+
+}
+
+function create_new_trip(){
+  amro_trip = (None,datetime.datetime.now(),'','','','','20')
+  app.Insert("/delivery", (req, res) => {
+    let sql = ('INSERT INTO amro_trips VALUES (?,?,?,?,?,?,?);', amro_trip)
     db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({ "error": err.message })
@@ -124,11 +282,26 @@ function rooms() {
   })
 }
 
+function canceldelivery(userId, tripId){
+  tripdata = (userId, 120,tripId)
+  app.patch("/cancel-delivery", (req, res) => {
+    let sql = ('UPDATE amro_trips SET RecievedBy=?,StatusId=? where Id=?;',tripdata)
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.json({
+        "data": rows
+      })
+    })
+  })
 
-function users() {
-  app.get("/users", (req, res) => {
-    let sql = `SELECT CreatedOn FROM  users`;
-    var params = []
+}
+
+function move(){
+  app.get("/move", (req, res) => {
+    let sql = 'SELECT X,Y FROM face_tracking ORDER BY Id DESC LIMIT 1;'
     db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({ "error": err.message })
@@ -139,7 +312,7 @@ function users() {
         "data": rows
       })
     })
-  })
+})
 }
 
 
